4775f58e40d54f42c83b695793e31e217104391a
Max Bye <>
2019-02-13 04:48:01 +0000

    untested end of the store

--- js/mechanics.js
+++ js/mechanics.js
@@ -11,9 +11,42 @@ function addAddition(arr){
 function getName(){
   $('.gender').text(localStorage.getItem('gender'));
   if(localStorage.getItem('gender') === 'man'){
-    return 'John';
+    $('.name').html('John');
+  } else {
+    $('.name').html('Mary');
   }
-  return 'Mary';
+}
+
+function checkWeapon(){
+  var hammer = charStatus.indexOf('arm');
+  var smashBottle = charStatus.indexOf('smash_bottle');
+  var bottle = charStatus.indexOf('bottle');
+  if(hammer > -1 || smashBottle > -1 || bottle > -1){  
+    $('.weaponResult').html('You feel the weapon you have already is better.');
+  } else{
+    $('.weaponResult').html('At a stretch, you think, this could be used as a weapon.');
+  }
+}
+
+function goneWrong(){
+  var index = charStatus.indexOf('arm');
+  
+  if(localStorage.getItem('gender') === 'man'){
+    var name = 'John'
+  } else {
+    name = 'Mary';
+  }
+  if (index > -1) {
+    $('.goneWrong').html('By the time more gauze is applied they have lost consciousness. You feel like you could have done a better job here.'); 
+    $('.option1').html('Leave ' + name); 
+    $('.option1').attr('onclick', 'tellStory(content, "MAX INSERT LEAVE")');
+    $('.option2').html('Take ' + name); 
+    $('.option1').attr('onclick', 'tellStory(content, 58)');
+  } else{
+    $('.goneWrong').html("Seeing the situation worsen. You wrap the material back across the wound and apply pressure. " + name + "'s skin is as pale as the white floor laminate, you better not make any more mistakes."); 
+    $('.option2').html('Continue'); 
+    $('.option1').attr('onclick', 'tellStory(content, 56)');
+  }
 }
 
 
